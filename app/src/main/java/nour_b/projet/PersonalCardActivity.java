5c6ac1efd649d3238af100b462cac311a99052f9
khadidja <khadidja.hamza6@gmail.com>
2017-05-25 20:55:28 +0200

    import contact finalisé

--- app/src/main/java/nour_b/projet/PersonalCardActivity.java
+++ app/src/main/java/nour_b/projet/PersonalCardActivity.java
@@ -1,204 +1,198 @@
-package nour_b.projet;
+    package nour_b.projet;
 
-import android.content.ActivityNotFoundException;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.telephony.SmsManager;
+    import android.content.ActivityNotFoundException;
+    import android.content.ContentResolver;
+    import android.content.Intent;
+    import android.graphics.Bitmap;
+    import android.graphics.BitmapFactory;
+    import android.net.Uri;
+    import android.os.Bundle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.util.Log;
+    import android.view.Menu;
+    import android.view.MenuItem;
+    import android.view.View;
+    import android.widget.ImageView;
+    import android.widget.TextView;
+    import android.widget.Toast;
+    import android.telephony.SmsManager;
 
-import java.io.File;
+    import java.io.File;
 
-import nour_b.projet.localDatabase.DBRegister;
-import nour_b.projet.model.User;
-import nour_b.projet.utils.SMSHandler;
+    import nour_b.projet.localDatabase.DBRegister;
+    import nour_b.projet.model.User;
+    import nour_b.projet.utils.SMSHandler;
 
-import static nour_b.projet.utils.ErrorMessages.pbGeolocalisation;
-import static nour_b.projet.utils.GPSHandler.getGeoLocation;
+    import static nour_b.projet.utils.ErrorMessages.pbGeolocalisation;
+    import static nour_b.projet.utils.GPSHandler.getGeoLocation;
 
-public class PersonalCardActivity extends AppCompatActivity {
+    public class PersonalCardActivity extends AppCompatActivity {
 
-    ImageView photo;
-    TextView address;
-    TextView mail;
-    TextView name;
-    TextView surname;
-    TextView birth;
-    TextView phone1;
-    TextView phone2;
-    TextView website;
+        ImageView photo;
+        TextView address;
+        TextView mail;
+        TextView name;
+        TextView surname;
+        TextView birth;
+        TextView phone1;
+        TextView phone2;
+        TextView website;
 
-    ImageView qr_code;
-    boolean sms = true;
-    User user;
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_card);
+        ImageView qr_code;
+        boolean sms = true;
+        User user;
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_personal_card);
 
-        photo = (ImageView) findViewById(R.id.photo);
-        name = (TextView) findViewById(R.id.name);
-        surname = (TextView) findViewById(R.id.surname);
-        mail = (TextView) findViewById(R.id.email);
-        birth = (TextView) findViewById(R.id.birth);
-        address = (TextView) findViewById(R.id.address);
-        phone1 = (TextView) findViewById(R.id.phone1);
-        phone2 = (TextView) findViewById(R.id.phone2);
-        website = (TextView) findViewById(R.id.site);
+            photo = (ImageView) findViewById(R.id.photo);
+            name = (TextView) findViewById(R.id.name);
+            surname = (TextView) findViewById(R.id.surname);
+            mail = (TextView) findViewById(R.id.email);
+            birth = (TextView) findViewById(R.id.birth);
+            address = (TextView) findViewById(R.id.address);
+            phone1 = (TextView) findViewById(R.id.phone1);
+            phone2 = (TextView) findViewById(R.id.phone2);
+            website = (TextView) findViewById(R.id.site);
 
-        Bundle bundle = getIntent().getExtras();
+            Bundle bundle = getIntent().getExtras();
 
-        if(bundle.getString("eMAIL")!= null) {
+            if(bundle.getString("eMAIL")!= null) {
 
-            DBRegister db = new DBRegister(this);
-            user = db.getUser(bundle.getString("eMAIL"));
+                DBRegister db = new DBRegister(this);
+                user = db.getUser(bundle.getString("eMAIL"));
 
-            if(user.getPhoto() != null) {
-                File imgFile = new File(user.getPhoto());
-                if(imgFile.exists()){
-                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-                    photo.setImageBitmap(myBitmap);
-                }
-            }
-
-            name.setText(user.getName());
-            surname.setText(user.getSurname());
-            mail.setText(user.getMail());
-            birth.setText(user.getBirth());
-            address.setText(user.getAddress());
-            phone1.setText(user.getTel1());
-            phone2.setText(user.getTel2());
-            website.setText(user.getWebsite());
-        }
-
-        address.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    startActivity(getGeoLocation(address.getText().toString()));
-                } catch (android.content.ActivityNotFoundException ex) {
-                    pbGeolocalisation(getApplicationContext());
-                }
-            }
-        });
-
-        qr_code = (ImageView) findViewById(R.id.qr_code);
-
-        qr_code.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    // startActivity();
-                } catch (android.content.ActivityNotFoundException ex) {
-                    //
-                }
-            }
-        });
-    }
-
-    // Mise en place du menu dans l'ActionBar (items répertoriés dans menu_main.xml)
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    // Gestion des actions à réaliser selon choix d'un item du menu
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.action_sms) {
-            try {
-                startActivityForResult(SMSHandler.getContact(), SMSHandler.PICK_CONTACT_REQUEST);
-                onActivityResult(SMSHandler.PICK_CONTACT_REQUEST,SMSHandler.CONTACT_PICKER , SMSHandler.getContact());
-                Toast.makeText(PersonalCardActivity.this, "Finished sending SMS...", Toast.LENGTH_SHORT).show();
-            } catch (ActivityNotFoundException ex) {
-                Toast.makeText(PersonalCardActivity.this, "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
-            }
-            return true;
-        }
-        if (id == R.id.action_import) {
-            sms = false;
-            try {
-                startActivityForResult(SMSHandler.getContact(), SMSHandler.PICK_CONTACT_REQUEST);
-
-                onActivityResult(SMSHandler.PICK_CONTACT_REQUEST,SMSHandler.CONTACT_PICKER , SMSHandler.getContact());
-                Toast.makeText(PersonalCardActivity.this, "Finished sending SMS...", Toast.LENGTH_SHORT).show();
-            } catch (ActivityNotFoundException ex) {
-                Toast.makeText(PersonalCardActivity.this, "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
-            }
-            return true;
-        }
-
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(PersonalCardActivity.this, RegisterActivity.class);
-            intent.putExtra("eMAIL", mail.getText().toString());
-            startActivity(intent);
-            return true;
-        }
-
-        if (id == R.id.action_login) {
-            MainActivity.LOGIN = false;
-            Intent intent = new Intent(PersonalCardActivity.this, MainActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // check whether the result is ok
-        Log.i("le resultaaa===> ", "" + resultCode);
-        if (resultCode == RESULT_OK) {
-            // Check for the request code, we might be usign multiple
-            // startActivityForReslut
-            switch (requestCode) {
-                case SMSHandler.CONTACT_PICKER :
-                    if(sms){
-                        ContentResolver cr = getContentResolver();
-                        String tel = SMSHandler.contactPicked(data, cr).getTel1();
-                        Log.i("le tel est ","==>"+user.toString());
-
-                        Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + tel));
-                        smsIntent.putExtra("sms_body",user.toString(getApplicationContext()));
-                        startActivity(smsIntent);
-                        break;
-                    }else{
-                        ContentResolver cr = getContentResolver();
-                        User user = SMSHandler.contactPicked(data, cr);
-                        Log.i("le tel est ","==>"+user.toString());
-                        name.setText(user.getName());
-                        surname.setText(user.getSurname());
-                        mail.setText(user.getMail());
-                        birth.setText(user.getBirth());
-                        address.setText(user.getAddress());
-                        phone1.setText(user.getTel1());
-                        phone2.setText(user.getTel2());
-                        website.setText(user.getWebsite());
-                      /*  Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + tel));
-                        smsIntent.putExtra("sms_body",user.toString());
-                        startActivity(smsIntent);*/
-                        break;
+                if(user.getPhoto() != null) {
+                    File imgFile = new File(user.getPhoto());
+                    if(imgFile.exists()){
+                        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                        photo.setImageBitmap(myBitmap);
                     }
+                }
 
+                name.setText(user.getName());
+                surname.setText(user.getSurname());
+                mail.setText(user.getMail());
+                birth.setText(user.getBirth());
+                address.setText(user.getAddress());
+                phone1.setText(user.getTel1());
+                phone2.setText(user.getTel2());
+                website.setText(user.getWebsite());
             }
-        } else {
-            Log.e("MainActivity", "Failed to pick contact");
+
+            address.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        startActivity(getGeoLocation(address.getText().toString()));
+                    } catch (android.content.ActivityNotFoundException ex) {
+                        pbGeolocalisation(getApplicationContext());
+                    }
+                }
+            });
+
+            qr_code = (ImageView) findViewById(R.id.qr_code);
+
+            qr_code.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        // startActivity();
+                    } catch (android.content.ActivityNotFoundException ex) {
+                        //
+                    }
+                }
+            });
+        }
+
+        // Mise en place du menu dans l'ActionBar (items répertoriés dans menu_main.xml)
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu, menu);
+            return true;
+        }
+
+        // Gestion des actions à réaliser selon choix d'un item du menu
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+
+            int id = item.getItemId();
+
+            if (id == R.id.action_sms) {
+                try {
+                    startActivityForResult(SMSHandler.getContact(), SMSHandler.PICK_CONTACT_REQUEST);
+                    onActivityResult(SMSHandler.PICK_CONTACT_REQUEST,SMSHandler.CONTACT_PICKER , SMSHandler.getContact());
+                    Toast.makeText(PersonalCardActivity.this, "Finished sending SMS...", Toast.LENGTH_SHORT).show();
+                } catch (ActivityNotFoundException ex) {
+                    Toast.makeText(PersonalCardActivity.this, "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+            if (id == R.id.action_import) {
+                sms = false;
+                try {
+                    startActivityForResult(SMSHandler.getContact(), SMSHandler.PICK_CONTACT_REQUEST);
+
+                    onActivityResult(SMSHandler.PICK_CONTACT_REQUEST,SMSHandler.CONTACT_PICKER , SMSHandler.getContact());
+                    Toast.makeText(PersonalCardActivity.this, "Finished importing contact...", Toast.LENGTH_SHORT).show();
+                } catch (ActivityNotFoundException ex) {
+                    Toast.makeText(PersonalCardActivity.this, "import contact faild, please try again later.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+
+            if (id == R.id.action_settings) {
+                Intent intent = new Intent(PersonalCardActivity.this, RegisterActivity.class);
+                intent.putExtra("eMAIL", mail.getText().toString());
+                startActivity(intent);
+                return true;
+            }
+
+            if (id == R.id.action_login) {
+                MainActivity.LOGIN = false;
+                Intent intent = new Intent(PersonalCardActivity.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            // check whether the result is ok
+            if (resultCode == RESULT_OK) {
+                // Check for the request code, we might be usign multiple
+                // startActivityForReslut
+                switch (requestCode) {
+                    case SMSHandler.CONTACT_PICKER :
+                        if(sms){
+                            ContentResolver cr = getContentResolver();
+                            String tel = SMSHandler.contactPicked(data, cr).getTel1();
+                            Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + tel));
+                            smsIntent.putExtra("sms_body",user.toString(getApplicationContext()));
+                            startActivity(smsIntent);
+                            break;
+                        }else{
+                            ContentResolver cr = getContentResolver();
+                            User user = SMSHandler.contactPicked(data, cr);
+                            name.setText(user.getName());
+                            surname.setText(user.getSurname());
+                            mail.setText(user.getMail());
+                            birth.setText(user.getBirth());
+                            address.setText(user.getAddress());
+                            phone1.setText(user.getTel1());
+                            phone2.setText(user.getTel2());
+                            website.setText(user.getWebsite());
+                            break;
+                        }
+
+                }
+            } else {
+                Log.e("MainActivity", "Failed to pick contact");
+            }
         }
     }
-}
--- app/src/main/java/nour_b/projet/utils/SMSHandler.java
+++ app/src/main/java/nour_b/projet/utils/SMSHandler.java
@@ -7,6 +7,12 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.util.Log;
 
+import com.google.zxing.common.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import nour_b.projet.model.User;
 
 public class SMSHandler {
@@ -44,12 +50,18 @@ public class SMSHandler {
             String name = cur.getString(cur
                     .getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
             Log.i("le nom===>", "" + name); // print data
+            user.setName(name);
+            // column index of the contact name
+            String surname = cur.getString(cur
+                    .getColumnIndex(ContactsContract.Contacts.PHONETIC_NAME));
+            Log.i("le nom===>", "" + name); // print data
+            user.setSurname(surname);
             // column index of the phone number
             Cursor pCur = cr.query(
                     ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                     ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                     new String[] { id }, null);
-            user.setName(name);
+
             while (pCur.moveToNext()) {
                 String phone = pCur
                         .getString(pCur
@@ -59,6 +71,7 @@ public class SMSHandler {
                 user.setTel1(phone);
             }
             pCur.close();
+
             // column index of the email
             Cursor emailCur = cr.query(
                     ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
@@ -85,33 +98,44 @@ public class SMSHandler {
                 String adr = adrCur
                         .getString(adrCur
                                 .getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET));
-                Log.i("le email ===>", "" + adr); // print data
+                Log.i("l'adresse  ===>", "" + adr); // print data
                 user.setAddress(adr);
             }
             adrCur.close();
             // column index of the webSite
-            Cursor cursorWebsite = cr.query(
-                    ContactsContract.Data.CONTENT_URI,
-                    null,
-                    ContactsContract.CommonDataKinds.Website.CONTACT_ID + " = ?",
-                    new String[] { id },
-                    null);
-            if (cursorWebsite != null) {
-                while (cursorWebsite.moveToNext()) {
-                    int i = cursorWebsite.getColumnIndex(ContactsContract.CommonDataKinds.Website.URL);
-                    int type = cursorWebsite.getColumnIndex(ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
-                    String s = cursorWebsite.getString(i);
-                    switch (type) {
-                        case ContactsContract.CommonDataKinds.Website.TYPE_HOME:
-                            // s *** private url not working
-                            break;
-                        case ContactsContract.CommonDataKinds.Website.TYPE_WORK:
-                            // s *** business url not working
-                            break;
-                    }
-                }
+            String[] cols = { ContactsContract.CommonDataKinds.Website.URL };
+            String filter = ContactsContract.Data.CONTACT_ID+" = ? " +
+                    " and "+ContactsContract.Data.MIMETYPE
+                    +" = '"+ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE+"'";
+            String[] params = { String.valueOf(id) };
+            Cursor website = cr.query(ContactsContract.Data.CONTENT_URI, cols, filter, params, null);
+
+            while (website.moveToNext()) {
+                String val = website.getString(website.getColumnIndex(ContactsContract.CommonDataKinds.Website.URL));
+              Log.i("le webSite est ==>",val);
+                user.setWebsite(val);
             }
-                cursorWebsite.close();
+
+            Date date = null;
+            String whereName = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.Event.TYPE + " = ?";
+            String[] whereNameParams = new String[] { id, ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE, String.valueOf(ContactsContract.CommonDataKinds.Event.TYPE_BIRTHDAY) };
+            Cursor c = null;
+            try {
+                c = cr.query(ContactsContract.Data.CONTENT_URI, null, whereName, whereNameParams, null);
+                if (c != null && c.moveToFirst()) {
+                    int indexBirthday = c.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Event.START_DATE);
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+                    date = format.parse(c.getString(indexBirthday));
+                }
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+            if(date != null) {
+                user.setBirth(date.toString());
+            }
+            if(c != null){
+                c.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
